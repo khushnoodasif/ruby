a8ffb150648f5841c46f980502e6a0ae6fc07c72
Khushnood Asif <khushnood99@gmail.com>
2022-02-22 11:08:42 +0000

    Create exercise1.rb

---
+++ makers/exercise1.rb
@@ -0,0 +1,11 @@
+# You should get line 2 to evaluate to 9
+p 3 * 3
+ 
+# Get line 4 to evaluate to "Hi there!"
+p "Hi there!"
+ 
+# Fix this so that it returns true (as a boolean) You might need to google what .includes does for ruby strings
+p "hi".include?("i") 
+ 
+# Waht's wrong with the line below? It should print out "HIYA"
+p "HIYA"
